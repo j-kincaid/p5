8870ee87ab58b70c58a3006ec942b9d868710a28
Jessica Kincaid <4j.kincaid@gmail.com>
2026-05-24 12:02:17 -0500

    add shapes to canvas

--- sketch.js
+++ sketch.js
@@ -3,6 +3,13 @@ function setup() {
 }
 
 function draw() {
-  background(255, 255, 0);
+  background(0);
+  rect(100, 200, 75, 125);
+  ellipse(200, 400, 75, 125);
+  triangle(30, 75, 58, 20, 86, 75);
+  quad(360, 150, 80, 80, 80, 70, 360, 70);
+  line();
+  arc();
   console.log(mouseX);
+  console.log(mouseY);
 }
