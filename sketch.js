696d45780de0517ad38c2f7bd5d56c96fe4e1387
Jessica Kincaid <4j.kincaid@gmail.com>
2026-05-24 12:21:28 -0500

    add color, stroke, fill to shapes

--- sketch.js
+++ sketch.js
@@ -3,12 +3,23 @@ function setup() {
 }
 
 function draw() {
+  // background layer goes behind the others
   background(0);
+  // rectangle
   rect(100, 200, 75, 125);
+  // ellipse
   ellipse(200, 400, 75, 125);
+  stroke(0, 0, 255);
+  fill(0);
+  // triangle
   triangle(30, 75, 58, 20, 86, 75);
+  // quad
   quad(360, 150, 80, 80, 80, 70, 360, 70);
-  line();
+  stroke(15, 0, 255);
+  // fill can use alpha 
+  fill(85, 0, 255, 100);
+  strokeWeight(6);
+  line(130, 120, 185, 175);
   arc();
   console.log(mouseX);
   console.log(mouseY);
