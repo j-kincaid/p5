e37d0dad34d61706ec287ed12a50d4d3f5b35c5d
Jessica Kincaid <4j.kincaid@gmail.com>
2026-05-30 15:37:35 -0500

    create randomly colored dots on black background

--- sketch.js
+++ sketch.js
@@ -1,22 +1,20 @@
-let squareSize;
-let lineWidth;
+let x, y, r, g, b;
 
 function setup() {
   // happens once
   createCanvas(1777, 1000);
   // lineWidth = random(8, 20);
-  background(100);
+  background(0);
 }
-
 
 function draw() {
   // this function LOOPS
-
-  lineWidth = random(3, 40);
-  squareSize = random(7, 300);
-  rectMode(CENTER);
-  strokeWeight(lineWidth);
-  stroke(0, 0, 255, 10);
-  fill(255, 255, 0, 18);
-  square(200, 150, squareSize);
+  r = random(255);
+  g = random(255);
+  b = random(255);
+  x = random(0, 1777); 
+  y = random(0, 1000); 
+  noStroke();
+  fill(r, g, b, 100);
+  circle(x, y, 24);
 }
