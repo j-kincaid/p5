12e4a7bf7c144a18b11d38b122f92c4200736a4c
Jessica Kincaid <4j.kincaid@gmail.com>
2026-05-30 14:57:51 -0500

    make circle white and make radius grow

--- sketch.js
+++ sketch.js
@@ -1,50 +1,22 @@
-var circleX = 0;
-var squareY = 15;
-var fillG = 100;
-var strokeB = 0;
 
+let circleX = 100;
+let rad = 100; 
 function setup() {
   // happens once
-  createCanvas(1280, 720);
-  
+  createCanvas(400, 300);
+}
+
+function mousePressed() {
+  circleX = 4;
 }
 
 function draw() {
   // this function LOOPS
   // background layer goes behind the others
   background(0);
-
-  // ellipse
-  stroke(15, 0, strokeB);
-  fill(250, 255, 0, 80);
-  ellipse(circleX, 450, 75, 75);
-  circleX = circleX + 1;
-  
-  // rectangle
-  rect(100, squareY, 75, 125);
-  squareY+=2;
-  //yellow fill
-  fill(0, fillG, fillG);
-  fillG-=1;
-  // triangle
-  triangle(30, 75, 58, 20, 86, 75);
-  // // quad
-  quad(360, 150, 80, 80, 80, 70, 360, 70);
-  stroke(15, 0, strokeB);
-  strokeB += 1;
-  // // ellipse
-  ellipse(600, 500, 175, 125);
-  stroke(0, 0, 255);
-  // // fill can use alpha 
-  fill(85, 0, 255, 100);
-  strokeWeight(6);
-  line(130, 120, 185, 175);
-  arc();
-  console.log(mouseX);
-  console.log(mouseY);
-}
-
-function mousePressed() {
-  // an EVENT: waits for something to happen
-  background(0);
+  noStroke();
+  fill(255);
+  circle(circleX, 10, rad);
+  circleX += 0.5;
+  rad += 0.15;
 }
